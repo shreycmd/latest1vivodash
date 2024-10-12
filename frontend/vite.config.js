ba6750a1537c6ef969d7c0200cb95398829dd46c
shreycmd <shrey03505@gmail.com>
2024-10-12 19:04:04 +0530

    back commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-
-})
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'server-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'server-cert.pem')),
+    },
+    host: '0.0.0.0',
+    port: 443,
+  },
+});
