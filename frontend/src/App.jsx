2aba56c8b3b24d1c891eb78d5d4b8933ff27c3e6
shreycmd <shrey03505@gmail.com>
2024-10-12 17:29:11 +0530

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Layout from './component/layout';
+import Layout from './component/Layout';
 import Product from './component/Products/Product';
 import Citem from './component/Citem';
 import Campaign from './component/Campaign';
